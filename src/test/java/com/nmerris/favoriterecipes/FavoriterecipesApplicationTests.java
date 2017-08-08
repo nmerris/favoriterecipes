42724ed52d26ab2a9b644254cc31b0287ca7a24b
Nathan Merris <joorge.jetson@gmail.com>
2017-08-07 21:14:58 -0400

    initial commit

---
+++ src/test/java/com/nmerris/favoriterecipes/FavoriterecipesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nmerris.favoriterecipes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FavoriterecipesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
